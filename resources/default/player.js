81c7898592470610c8c4286fe6f418cf08cbf32d
Jim <pcarranza@gmail.com>
2011-01-25 21:12:59 +0100

    Volume icon make more sense for me now

--- resources/default/player.js
+++ resources/default/player.js
@@ -254,13 +254,13 @@ function update_status() {
 		} else {
 			muted = false;
 			$('#vol_status').attr('title', 'Volume: ' + (volume * 100) + '%')
-			if (volume >= 0.81) {
+			if (volume > 0.79) {
 				// high
 				$('#vol_status').attr('src', 'img/volume-high.png');
-			} else if (volume >= 0.51) {
+			} else if (volume > 0.49) {
 				// medium
 				$('#vol_status').attr('src', 'img/volume-medium.png');
-			} else if (volume >= 0.11) {
+			} else if (volume > 0.19) {
 				// low
 				$('#vol_status').attr('src', 'img/volume-low.png');
 			} else {
