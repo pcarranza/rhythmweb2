9e3491541a120d47d2c32cf37befb754a711c8d2
Pablo Carranza <pcarranza@gmail.com>
2014-06-14 19:08:14 -0400

    Refactor request Resource Handler for simplification

--- serve/request.py
+++ serve/request.py
@@ -1,85 +1,50 @@
 import os
 import io
 from datetime import timedelta, datetime
+from collections import defaultdict
 
 import logging
 log = logging.getLogger(__name__)
 
 class ResourceHandler(object):
 
-    __content_type = None
-    __open_as = None
-    __file = None
-    __extension = None
-
-    def __init__(self, resource, content_type=None, open_as=''):
-        try:
-            log.debug('Creating ResourceHandler Instance for resource %s' % resource)
-
-            self._content_type = content_type
-            self.__open_as = open_as
-
-            self.__file = resource
-            self.__extension = str(os.path.splitext(self.__file)[1]).lower()
-
-            log.debug('Resource %s file is %s' % (resource, self.__file))
-        except:
-            log.error('Exception initializing resource handler')
+    def __init__(self, resource):
+        log.debug('Creating ResourceHandler for resource %s' % resource)
+        self.resource = resource
+        self.extension = str(os.path.splitext(self.resource)[1]).lower()
+        self.content_types = {
+            '.css': ('text/css', 't'),
+            '.htm': ('text/html', 't'),
+            '.html': ('text/html', 't'),
+            '.gif': ('image/gif', 'b'),
+            '.png': ('image/png', 'b'),
+            '.jpg': ('image/jpeg', 'b'),
+            '.jpeg': ('image/jpeg', 'b'),
+            '.ico': ('image/ico', 'b'),
+            '.svg': ('image/svg+xml', 't'),
+            '.js': ('application/x-javascript', 't'),
+        }
 
     def handle(self, response, accept_gzip=False):
-        try:
-            log.debug('Handling resource %s' % self.__file)
+        log.debug('Handling resource %s' % self.resource)
 
-            (content_type, open_as) = self.__get_content_type()
+        (content_type, open_as) = self.content_types.get(self.extension,
+                ('text/plain', 't'))
 
-            mtime = os.path.getmtime(self.__file)
-            mtime = datetime.fromtimestamp(mtime)
-            expiration = datetime.now() + timedelta(days=365)
+        mtime = os.path.getmtime(self.resource)
+        mtime = datetime.fromtimestamp(mtime)
+        expiration = datetime.now() + timedelta(days=365)
 
-            headers = [("Content-type", content_type), \
-                       ('Cache-Control', 'public'), \
-                       ('Last-Modified', mtime.ctime()), \
-                       ('Expires', expiration.ctime())]
+        headers = [("Content-type", content_type), \
+                    ('Cache-Control', 'public'), \
+                    ('Last-Modified', mtime.ctime()), \
+                    ('Expires', expiration.ctime())]
 
-            open_mode = 'r%s' % open_as
+        open_mode = 'r%s' % open_as
 
-            with open(self.__file, open_mode) as f:
-                response('200 OK', headers)
-                return f.read()
-        except:
-            log.error('Exception handling resource {}'.format(self.__file), exc_info=True)
-            return ''
-
-    def __get_content_type(self):
-        if not self.__content_type:
-            (self.__content_type, self.__open_as) = self.__content_type_by_ext(self.__extension)
-
-        log.debug('Returning content type %s' % self.__content_type)
-        return (self.__content_type, self.__open_as)
-
-    def __content_type_by_ext(self, ext):
-        if ext == '.css':
-            return ('text/css', 't')
-        if ext == '.htm':
-            return ('text/html', 't')
-        if ext == '.html':
-            return ('text/html', 't')
-        if ext == '.gif':
-            return ('image/gif', 'b')
-        if ext == '.png':
-            return ('image/png', 'b')
-        if ext == '.jpg':
-            return ('image/jpeg', 'b')
-        if ext == '.jpeg':
-            return ('image/jpeg', 'b')
-        if ext == '.ico':
-            return ('image/ico', 'b')
-        if ext == '.svg':
-            return ('image/svg+xml', 't')
-        if ext == '.js':
-            return ('application/x-javascript', 't')
-
-        return ('text/plain', 't')
+        with open(self.resource, open_mode) as f:
+            response('200 OK', headers)
+            return f.read()
 
 
 class ServerException(Exception):
--- test/test_web_resources.py
+++ test/test_web_resources.py
@@ -18,8 +18,23 @@ class TestWebStatus(unittest.TestCase):
         result = handle_request(self.app, environ(''), self.response)
         self.assertIn('<html>', result)
 
+    def test_load_index_by_full_name(self):
+        result = handle_request(self.app, environ('/index.html'), self.response)
+        self.assertIn('<html>', result)
+
+    def test_load_style(self):
+        result = handle_request(self.app, environ('/style.css'), self.response)
+        self.assertIsNotNone(result)
+
+    def test_load_style(self):
+        result = self.app.handle_request(environ('/img/star.png'), self.response)
+        self.assertIsNotNone(result)
+
     def test_not_found(self):
         result = handle_request(self.app, environ('invalid_file'), self.response)
         self.assertEquals('ERROR: Could not find resource /invalid_file', result)
         self.response.assert_called_with('404 Could not find resource /invalid_file', 
                 [('Content-type', 'text/html; charset=UTF-8')])
+
+
+
